d4a4660659284bfdfe2570d26cdcb02d2e401ae1
Steve Lacerda <46729305+StevenLacerda@users.noreply.github.com>
2021-01-12 15:20:27 -0800

    4.9.0

--- src/main/java/com/example/cassandra/TestingConnections.java
+++ src/main/java/com/example/cassandra/TestingConnections.java
@@ -1,80 +1,26 @@
 package com.example.cassandra;
-import com.datastax.driver.core.Cluster;
-import com.datastax.driver.core.Host;
-import com.datastax.driver.core.Metadata;
-import com.datastax.driver.core.Metrics;
-import com.datastax.driver.core.ResultSet;
-import com.datastax.driver.core.Row;
-import com.datastax.driver.core.Session;
-import com.datastax.driver.core.policies.DCAwareRoundRobinPolicy;
-import com.datastax.driver.core.policies.LatencyAwarePolicy;
-import com.datastax.driver.core.policies.RoundRobinPolicy;
-//import com.datastax.driver.core.policies.DefaultRetryPolicy;
-import com.datastax.driver.core.QueryOptions;
-import com.datastax.driver.core.ConsistencyLevel;
-//import com.datastax.driver.core.policies.RetryPolicy;
-import com.datastax.driver.core.policies.DowngradingConsistencyRetryPolicy;
-import com.datastax.driver.core.policies.WhiteListPolicy;
-import java.util.Collection;
-import java.net.InetSocketAddress;
-import java.util.ArrayList;
-import com.datastax.driver.core.BatchStatement;
-import com.datastax.driver.core.SimpleStatement;
-import com.datastax.driver.core.PreparedStatement;
-import com.datastax.driver.core.BoundStatement;
 
+import com.datastax.oss.driver.api.core.CqlSession;
+import com.datastax.oss.driver.api.core.cql.*;
+
+import java.net.InetSocketAddress;
 
 public class TestingConnections {
-    private Cluster cluster;
-    private Session session;
+    public CqlSession session;
 
     public void connect(String nodeIp, String user, String pwd) {
             System.out.printf("User: %s", user);
             System.out.printf("Password: %s", pwd);
             
-            Collection<InetSocketAddress> whiteList = new ArrayList<InetSocketAddress>();
-//            whiteList.add(new InetSocketAddress("10.101.32.110", 9042));
-//            whiteList.add(new InetSocketAddress("10.101.33.200", 9042));
-
-            cluster = Cluster.builder()
-                    .addContactPoint(nodeIp)
-//                    .withSSL()
-//                    ssl options to add to runtime env:
-//                    -Djavax.net.ssl.trustStore=/Users/stevelacerda/.ssh/lacerda_sa_5_1_truststore.jks
-//                    -Djavax.net.ssl.trustStorePassword=cassandra
+            session = CqlSession.builder()
+                    .addContactPoint(new InetSocketAddress("1.2.3.4", 9042))
                     
-                    
-                    
-                    .withQueryOptions(new QueryOptions()
-                        .setConsistencyLevel(ConsistencyLevel.LOCAL_QUORUM)
-                        .setSerialConsistencyLevel(ConsistencyLevel.LOCAL_SERIAL))
-//                    .withRetryPolicy(new CustomRetryPolicy(1,1,1))
-//                    .withLoadBalancingPolicy(new WhiteListPolicy(DCAwareRoundRobinPolicy.builder().withLocalDc("SearchAnalytics").withUsedHostsPerRemoteDc(1).allowRemoteDCsForLocalConsistencyLevel().build(), whiteList))
-//                    .withLoadBalancingPolicy(new WhiteListPolicy(new RoundRobinPolicy(), whiteList))
-
-                    .withCredentials("cassandra", "cassandra")
                     .build();
-
-            Metadata metadata = cluster.getMetadata();
-            Metrics metrics = cluster.getMetrics();
-            System.out.printf("Connected hosts: %s\n", metrics.getKnownHosts().getValue());
-
-            System.out.printf("Connected to cluster: %s\n", metadata.getClusterName());
-
-            for (Host host : metadata.getAllHosts()) {
-                    System.out.printf("Datacenter: %s, Host: %s, Rack: %s\n", host.getDatacenter(), host.getAddress(), host.getRack());
-            }
-
-            session = cluster.connect();
-                        
+            
             String keyspace = "keyspace1";
             String table = "names";
             this.getData(keyspace, table);
 //            this.writeData(keyspace, table);
-            
-        
-    
-    
     }
     
     public void getData(String keyspace, String table) {
@@ -139,7 +85,7 @@ public class TestingConnections {
     }
 
     public void close() {
-            cluster.close();
+            session.close();
     }
 
     public static void main(String[] args) {
